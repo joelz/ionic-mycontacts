661e98c2be7370cd1d333e6f67656d0439c10c57
Joel Zhang <rugoingdown@gmail.com>
2016-03-12 22:02:11 +0800

    side bar; multi page; ??????????reload everything; ???detail???

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,17 @@
 angular.module('starter.controllers', [])
 
+.controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
+
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
+
+
+})
+
 .controller('ChatsCtrl', function ($scope, Chats, $ionicLoading) {
     // With the new view caching in Ionic, Controllers are only called
     // when they are recreated or on app start, instead of every page change.
@@ -30,4 +42,18 @@ angular.module('starter.controllers', [])
 
 .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
     $scope.chat = Chats.get($stateParams.chatId);
-});
+})
+
+.controller('SettingsCtrl', function ($scope, Chats, $ionicLoading) {
+    $scope.refresh = function () {
+
+        $ionicLoading.show({
+            template: 'loading'
+        });
+
+        Chats.refresh(function () {
+            $ionicLoading.hide();
+        });
+    }
+})
+;
